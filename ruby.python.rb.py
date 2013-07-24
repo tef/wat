f10c0a1f2f55d386d32aa64bde55be2911e13f79
tef <thomas.figg@gmail.com>
2013-07-24 15:03:58 +0100

    adding python/ruby quine

---
+++ ruby.python.rb.py
@@ -0,0 +1,3 @@
+# Python or Ruby
+l,p,q=(""and"# Ruby"+10 .chr or"# Python"+chr(10)),'l,p,q=(""and"# Ruby"+10 .chr or"# Python"+chr(10))','print((""and"#{print l;c=39.chr;puts p+44.chr+c+p+c+44.chr+c+q+c;puts q}"or"{}{},{!r},{!r}{}{}".format(l,p,p,q,chr(10),q)))'
+print((""and"#{print l;c=39.chr;puts p+44.chr+c+p+c+44.chr+c+q+c;puts q}"or"{}{},{!r},{!r}{}{}".format(l,p,p,q,chr(10),q)))
